d94a5e13172cc520593115c5da4e255ba40e4149
박현도 <hyundo@wemakeprice.com>
2022-09-06 21:09:43 +0900

    init project

---
+++ src/main/java/com/landon/hcrawler/HCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.landon.hcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/landon/hcrawler/HCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landon.hcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
